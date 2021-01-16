19d6a975a5ea22058aa3f78d75ca15f21b9c888e
Flavius <flavius.petruca@stud.acs.upb.ro>
2021-01-16 16:09:48 +0200

    Second commit

--- firstProj/index.js
+++ firstProj/index.js
@@ -1,15 +1,28 @@
 const express = require('express');
-
+const dataStore = require('nedb');
+ 
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-const allData = [];
+const database  = new dataStore('database.db');
+database.loadDatabase();
 
 app.post('/api', (request, response) => {
   const data = request.body;
-  allData.push(data);
-  response.json(allData);
-  console.log(allData);
+  const ts = Date.now();
+  data.timestamp = ts;
+  //database.insert(data);
+  response.json(data)
+});
+
+app.post('/trial', (request, response) =>{
+  const data = request.body;
+  database.find({"text": data.text}, function(err, resp){
+    if(err)
+      response.json("CODUL NU ESTE VALID!");
+    else
+    response.json("CODUL ESTE VALID");
+  })
 });
